d08665c66d2b2714a02ae1b7af81e0e57bf09a91
Roger Taracha <rojtaracha@gmail.com>
2017-10-31 16:44:20 +0300

    [Routes] GET ALL: [+] A test to verify the fetched records. [+] Update the view to get the test to pass.

--- puppies/tests/test_views.py
+++ puppies/tests/test_views.py
@@ -9,3 +9,26 @@ from ..serializers import PuppySerializer
 
 # Initailize the APIClient app
 client = Client()
+
+
+class GetAllPuppiesTest(TestCase):
+    """Test module for GET all puppies API"""
+
+    def setUp(self):
+        Puppy.objects.create(
+            name='Moana', age=3, breed='Chihuahua', color='white')
+        Puppy.objects.create(
+            name='Chinku', age=5, breed='Shih Tzu', color='Brown')
+        Puppy.objects.create(
+            name='Pendo', age=4, breed='Poodle', color='Apricot')
+        Puppy.objects.create(
+            name='Rex', age=4, breed='German Shepherd', color='Brown')
+
+    def test_get_all_puppies(self):
+        # get API Response
+        response = client.get(reverse('get_post_puppies'))
+        # get data from DB
+        puppies = Puppy.objects.all()
+        serializer = PuppySerializer(puppies, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
