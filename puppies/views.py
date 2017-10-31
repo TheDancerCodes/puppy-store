3172e1dcacb2ecc8b2ee3c5d73d3560332e90a11
Roger Taracha <rojtaracha@gmail.com>
2017-10-31 12:08:46 +0300

    [FEATURE] Update Views: [+] Create a skeleton of all view functions that return empty responses. [+] Map them with their appropriate URLs.

--- puppies/views.py
+++ puppies/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Puppy
+from .serializers import PuppySerializer
 
-# Create your views here.
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_puppy(request, pk):
+    """Function handling single elements."""
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except Puppy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # GET details of a single puppy
+    if request.method == 'GET':
+        return Response({})
+    # DELETE a single puppy
+    elif request.method == 'DELETE':
+        return Response({})
+    # UPDATE details of a single puppy
+    elif request.method == 'PUT':
+        return Response({})
+
+
+@api_view(['GET', 'POST'])
+def get_post_puppies(request):
+    """Function handling collections."""
+
+    # get all puppies
+    if request.method == 'GET':
+        return Response({})
+    # insert a new record for a puppy
+    elif request.method == 'POST':
+        return Response({})
