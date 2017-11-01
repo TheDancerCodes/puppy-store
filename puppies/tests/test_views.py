354d3106e40a25adb0afd24f74eb62e52217cc2d
Roger Taracha <rojtaracha@gmail.com>
2017-11-01 10:41:22 +0300

    [FEATURE] Routes: POST. [+] Inserting a new record involves two cases as well:     [=] Inserting a valid puppy     [=] Inserting a invalid puppy
    [+] Update the view to get the tests to pass.

--- puppies/tests/test_views.py
+++ puppies/tests/test_views.py
@@ -61,8 +61,39 @@ class GetSinglePuppyTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
+class CreateNewPuppyTest(TestCase):
+    """Test module for inserting a new puppy"""
 
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Moana',
+            'age': 3,
+            'breed': 'Chihuahua',
+            'color': 'White'
+        }
 
+        self.invalid_payload = {
+            'name': '',
+            'age': 3,
+            'breed': 'Chihuahua',
+            'color': 'White'
+        }
+
+    def test_create_valid_puppy(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_puppy(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 
